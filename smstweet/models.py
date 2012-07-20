b486c3376d2d850baf71d4c8c81e6e99233c3a05
Francis Plaza <efcplaza@gmail.com>
2012-07-20 19:53:05 +0800

    Updated models

--- smstweet/models.py
+++ smstweet/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 class IncomingSMS(models.Model):
-    msgType = CharField('Message Type', max_length=5)
-    msgId = CharField('Message ID', max_length=50)
-    source = CharField('Source', max_length=11)
-    target = CharField('Target', max_length=8)
-    msg = CharField('Message', max_length=200)
-    udh = CharField('User Data Header', max_length=200)
+    msgType = models.CharField('Message Type', max_length=5)
+    msgId = models.CharField('Message ID', max_length=50)
+    source = models.CharField('Source', max_length=11)
+    target = models.CharField('Target', max_length=8)
+    msg = models.CharField('Message', max_length=200)
+    udh = models.CharField('User Data Header', max_length=200)
+    timestamp = models.DateField(auto_now_add=True)
 
     def __unicode__(self):
         return u'(%s) %s' % (self.source, self.msgType)
