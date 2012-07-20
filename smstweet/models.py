62beb553113b10818d90ba0fabfe0a093e160f79
Francis Plaza <efcplaza@gmail.com>
2012-07-20 19:59:01 +0800

    Updated IncomingSMS Model

--- smstweet/models.py
+++ smstweet/models.py
@@ -5,9 +5,9 @@ class IncomingSMS(models.Model):
     msgId = models.CharField('Message ID', max_length=50)
     source = models.CharField('Source', max_length=11)
     target = models.CharField('Target', max_length=8)
-    msg = models.CharField('Message', max_length=200)
-    udh = models.CharField('User Data Header', max_length=200)
-    timestamp = models.DateField(auto_now_add=True)
+    msg = models.TextField('Message', blank=True)
+    udh = models.TextField('User Data Header', blank=True)
+    timestamp = models.DateTimeField('Timestamp', auto_now_add=True)
 
     def __unicode__(self):
         return u'(%s) %s' % (self.source, self.msgType)
